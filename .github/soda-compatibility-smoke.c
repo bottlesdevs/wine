ce4b57ce77918dea8a2aafdad7c6e678a9b755ff
mirkobrombin <brombin94@gmail.com>
2026-08-30 20:35:27 +0200

    fix[closes #79]: limit Heroes VII CPU topology

--- .github/soda-compatibility-smoke.c
+++ .github/soda-compatibility-smoke.c
@@ -1,11 +1,19 @@
 #include <windows.h>
 #include <shellapi.h>
+#include <stdio.h>
 
 int wmain(int argc, WCHAR **argv)
 {
     HINSTANCE result;
+    SYSTEM_INFO info;
 
     if (argc != 2) return 2;
+    if (!lstrcmpW(argv[1], L"--processors"))
+    {
+        GetSystemInfo(&info);
+        wprintf(L"%lu\n", info.dwNumberOfProcessors);
+        return 0;
+    }
     result = ShellExecuteW(NULL, L"open", argv[1], NULL, NULL, SW_SHOWNORMAL);
     return (INT_PTR)result > 32 ? 0 : 1;
 }
